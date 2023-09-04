5f1b3af19ad77cc97aac19fb5f44c98289aa54af
Jens Andreasson <jens.andreasson@ntig.se>
2023-09-04 15:39:15 +0200

    ✨ penga exempel

--- iteration.js
+++ iteration.js
@@ -3,11 +3,6 @@ for (let i = 0; i < 10; i++) {
 }
 
 let names = ['Jens', 'Maja', 'Hanna', 'Björn']
-
-for (let index = 0; index < names.length; index++) {
-    console.log(names[index]);
-}
-
 names.forEach((name, index) => {
     console.log(name, index);
 });
@@ -16,6 +11,12 @@ for (let i = 0; i < 10; i++) {
     const roll = Math.ceil(Math.random() * 6);
     console.log(roll)
 }
-
 // skapa en array, där alla tärningsslag sparas
 // skriv ut hela arrayen array.push(värde)
+
+
+let rolls = [];
+for (let i = 0; i < 10; i++) {
+    // kod för att rulla tärning
+    rolls.push(1)
+}
---
+++ money.js
@@ -0,0 +1,9 @@
+// penga konverterare
+
+let amount = 100;
+
+// från sek till usd 11.09
+
+let exchangeRate = 11.09;
+
+console.log(amount * exchangeRate)
